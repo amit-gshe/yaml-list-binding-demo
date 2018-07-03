e4179158b722a05ef128c2931cbd2cec60fa8619
amit-gshe <scybhe@163.com>
2018-07-03 18:07:31 +0800

    first commit

---
+++ src/main/kotlin/com/example/yamllistbindingdemo/YamlListBindingDemoApplication.kt
@@ -0,0 +1,32 @@
+package com.example.yamllistbindingdemo
+
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
+
+@SpringBootApplication
+class YamlListBindingDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<YamlListBindingDemoApplication>(*args)
+}
+
+data class Printer(val deviceNo: String, val key: String)
+
+@Component
+@ConfigurationProperties
+class PrinterProperties {
+    var printers: List<Printer> = listOf()
+}
+
+@Component
+class Runner(val printerProperties: PrinterProperties) : ApplicationRunner {
+
+
+    override fun run(args: ApplicationArguments?) {
+        println(printerProperties.printers.size)
+    }
+}
